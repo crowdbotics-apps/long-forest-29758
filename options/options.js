593358cf7284fc45d6a8bc772d810cba145128b9
Crowdbotics <team@crowdbotics.com>
2021-08-12 13:44:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "long_forest_29758Identifier",
+  url: "https://long_forest_29758Identifier.botics.co",
+  api: "https://long_forest_29758Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
